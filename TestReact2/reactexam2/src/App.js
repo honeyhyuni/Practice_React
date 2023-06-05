bdd8453997488713e5d5eeba9a8c584f7cd608d6
seunghyeon lee <ska8417@onss.co.kr>
2023-06-05 13:56:47 +0900

    Add Practice_React Basic2

--- TestReact2/reactexam2/src/App.js
+++ TestReact2/reactexam2/src/App.js
@@ -78,9 +78,12 @@ function App() {
     const localData = localStorage.getItem('diary');
     if(localData){
       const diaryList = JSON.parse(localData).sort((a, b)=> parseInt(b.id) - parseInt(a.id));
-      dataId.current = diaryList[0].id + 1
 
-      dispatch({type:"INIT", data:diaryList})
+      if(diaryList.length >= 1){
+        dataId.current = diaryList[0].id + 1
+        dispatch({type:"INIT", data:diaryList})
+      }
+
     }
   }, [])
 
